c4da88138ad8303e975ce8ab1a4fd3aa6f2e8a05
Zenbook <syouran0508@gmail.com>
2025-02-15 01:15:54 +0800

    have to change the codes in tensorneat up to now

--- tensorneat-cartpolev1.py
+++ tensorneat-cartpolev1.py
@@ -8,7 +8,7 @@ Paper: https://arxiv.org/pdf/2404.01817
 from tensorneat.pipeline import Pipeline
 from tensorneat.algorithm.neat import NEAT
 from tensorneat.genome import DefaultGenome, BiasNode
-from tensorneat.problem.rl import BraxEnv
+from tensorneat.problem.rl import BraxEnv,GymNaxEnv
 from tensorneat.common import ACT, AGG
 
 # problem = BraxEnv(
@@ -37,6 +37,11 @@ problem = BraxEnv(
     max_step=1000,
 )
 
+# problem = GymNaxEnv( # not working
+#     env_name="CartPole-v1",
+#     repeat_times=5,
+# )
+
 algorithm = NEAT(
     pop_size=50,
     species_size=10,
@@ -45,20 +50,22 @@ algorithm = NEAT(
     genome=DefaultGenome(
         num_inputs=4,
         num_outputs=1,
+        max_nodes=10,
+        max_conns=20,
         init_hidden_layers=(),
         node_gene=BiasNode(
             activation_options=ACT.tanh,
             aggregation_options=AGG.sum,
         ),
         output_transform=ACT.tanh,
-    ),
+    )
 )
 
 pipeline = Pipeline(
     algorithm=algorithm,
     problem=problem,
     seed=42,
-    generation_limit=100,
+    generation_limit=2,
     fitness_target=1000,
 )
 
@@ -67,3 +74,27 @@ state = pipeline.setup()
 
 # Run until termination
 state, best = pipeline.auto_run(state)
+
+# show result
+pipeline.show(state, best, output_type="gif")
+
+# print(state.__class__) # tensorneat.common.state.State
+# print(state.registered_keys()) # dict_keys(['randkey', 'pop_nodes', 'pop_conns', 'generation', 'species'])
+# print(state.species.registered_keys()) # 'species_keys', 'best_fitness', 'last_improved', 'member_count', 'idx2species', 'center_nodes', 'center_conns', 'next_species_key'
+# print(best)  # tuple
+
+# visualize the best individual
+# genome = pipeline.algorithm.genome
+# network = genome.network_dict(None, *best)  # Transform the network from JAX arrays to a Python dict
+# genome.visualize(network, save_path="./network.png",with_labels=True)
+# print(algorithm.genome.repr(state, *best))
+# print(*best)
+
+# randkey_, randkey = jax.random.split(state.randkey)
+# state = state.update(randkey=randkey)
+# problem.show(
+#     state,
+#     randkey_,
+#     algorithm.forward,
+#     params
+# )
